c4f3baf67d286c7644d94ee2a554f454ee071531
Nahin-CDR <dev.nahin95@gmail.com>
2021-10-06 20:50:24 +0600

    image adding from internet

---
+++ lib/image_loading_from_net.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({ Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: const Text(
+            "Hello Flutter",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: Container(
+          child: Image.network(
+              "https://blogger.googleusercontent.com/img/a/AVvXsEj9EkW4aA8MRDpGPUbt-xZLIiwsWkLzCaRtE0_uf8wTU4UHjLM0vo03tlU0sT-_kH-o87K5ZyOAbgphDZHBAu4kCcw41o4uclnoc9ffg1uPif6x0-ZEedz1ZWA1m2N8ZFhSKXY5qdG_5rblhGu7CLtkmvkQuErYYHI8rS4GQKTcmzteXfeJ5HIwMU_V=s600",
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
